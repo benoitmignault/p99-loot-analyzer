ea277c02c6acd3ef301464e619fe8b74551904e4
Benoit Mignault <b.mignault@gmail.com>
2026-09-05 15:26:33 -0400

    Feat: Add support for charmed pet kill log messages

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def extract_slain_monster(line):
     # Il va arriver que le joueur ne soit pas celui qui a tué le monstre, mais un autre joueur,
     elif " has been slain by " in line:     
         return line.split("] ", 1)[1].split(" has been slain by ", 1)[0].strip()
+    
+    # 2026-09-05, une situation que je n'avais pas encore prise en compte, c'est lorsque le monstre est tué par un pet charmé
+    # Le message qu'on doit attraper : A kodiak bear tells you, 'Attacking a grizzly bear Master.'
+    elif " tells you, 'Attacking " in line and " Master.'" in line:
+        return line.split(" tells you, 'Attacking ")[1].split(" Master.'")[0].strip()
 
     return None
 
